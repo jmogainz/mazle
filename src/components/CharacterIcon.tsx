eaa1ab3871e8d3a241c4c87139115ef91b5a616d
Jacob Moore <jlmoors001@gmail.com>
2026-01-26 10:58:39 -0600

    update: penguin in

--- src/components/CharacterIcon.tsx
+++ src/components/CharacterIcon.tsx
@@ -52,8 +52,9 @@ export default function CharacterIcon({ characterId, skinId, size = 34, title, l
   }
 
   if (sId === 'penguin' && !locked) {
-    const PENGUIN_ICON_SIZE = 20;
+    const PENGUIN_ICON_SIZE = 26.9;
     const PENGUIN_ICON_OFFSET = (32 - PENGUIN_ICON_SIZE) / 2;
+    const PENGUIN_ICON_OFFSET_Y = PENGUIN_ICON_OFFSET - 0.5;
     return (
       <svg
         width={size}
@@ -69,7 +70,7 @@ export default function CharacterIcon({ characterId, skinId, size = 34, title, l
         <image
           href="/assets/images/penguin.svg"
           x={PENGUIN_ICON_OFFSET}
-          y={PENGUIN_ICON_OFFSET}
+          y={PENGUIN_ICON_OFFSET_Y}
           width={PENGUIN_ICON_SIZE}
           height={PENGUIN_ICON_SIZE}
           shapeRendering="geometricPrecision"
